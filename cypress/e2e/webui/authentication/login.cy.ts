c82a5025878e00f8a53a983addfcd9f288ede5e6
natezhengbne <donitzheng@gmail.com>
2024-05-29 11:37:55 +1000

    more login tests

--- cypress/e2e/webui/authentication/login.cy.ts
+++ cypress/e2e/webui/authentication/login.cy.ts
@@ -1,4 +1,4 @@
-import { createUser } from "../login.selector";
+import { createUser, generateRandomEmail } from "../login.selector";
 import {
 	DEFAULT_VALID_PASSWORD,
 	EXISTING_EMAIL,
@@ -6,27 +6,29 @@ import {
 } from "../constants";
 
 describe("exist user", () => {
-	context("login successfully", () => {
-		it.only("can finish login process", () => {
-			cy.visit(LOGIN_PAGE);
-			createUser({
-				user: {
-					email: EXISTING_EMAIL,
-				},
-				options: {
-					logoutAfterSignup: true,
-				},
-			});
+	beforeEach(() => {
+        // prepare the existing user
+		cy.visit(LOGIN_PAGE);
+		createUser({
+			user: {
+				email: EXISTING_EMAIL,
+			},
+			options: {
+				logoutAfterSignup: true,
+			},
+		});
 
-            cy.url().then(url => {
-                if (!url.includes(LOGIN_PAGE)) {
-                    cy.visit(LOGIN_PAGE);
-                } else {
-                    cy.reload();
-                }
+		cy.url().then((url) => {
+			if (!url.includes(LOGIN_PAGE)) {
+				cy.visit(LOGIN_PAGE);
+			} else {
+				cy.reload();
+			}
+		});
+	});
 
-            });
-
+	context("login success", () => {
+		it("can finish login process", () => {
 			cy.get("input#loginRegisterEmail_email").type(EXISTING_EMAIL);
 
 			cy.get("[data-id=loginRegisterEmail_submit]").click();
@@ -41,10 +43,22 @@ describe("exist user", () => {
 
 	context("but typo email", () => {
 		it("can redirect to login page when correct the email", () => {
-			cy.visit(LOGIN_PAGE);
-
-			cy.get("input#loginRegisterEmail_email").type(EXISTING_EMAIL);
+			cy.get("input#loginRegisterEmail_email").type(generateRandomEmail());
 			cy.get("[data-id=loginRegisterEmail_submit]").click();
+
+			cy.get("input#signup_form_email")
+				.should("be.visible")
+				.clear()
+				.type(EXISTING_EMAIL)
+				.blur();
+
+			cy.get("input#LoginRegister_Login_password")
+				.should("be.visible")
+				.type(DEFAULT_VALID_PASSWORD);
+
+			cy.get("[data-id=LoginRegister_Login_submitButton]").click();
+
+			cy.get("[data-id=myAccountPage_pageHeader]").should("be.visible");
 		});
 	});
 });
